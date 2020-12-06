101710620ff4192c271f2ecbf0aaec6c98a58bb9
dbroemme <darren.broemmer@gmail.com>
2020-12-06 08:25:21 -0500

    Add challenge 3

---
+++ problems/code3.rb
@@ -0,0 +1,9 @@
+require_relative 'helpers'  # DO NOT REMOVE THIS LINE
+#####################################################
+#   Challenge 3: Display a personal greeting
+#####################################################
+ 
+# Step 1. Ask the user for their name and remember the answer
+
+# Step 2. Display "Hello #{name}" to the screen
+
---
+++ problems/solution3.rb
@@ -0,0 +1,4 @@
+require_relative 'helpers'  # DO NOT REMOVE THIS LINE
+
+name = ask_me("What is your name?")
+tell_me("Hello #{name}")
