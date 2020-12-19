6f13fec43410f2fd2b694a4dc6712b3fa34476c0
dbroemme <darren.broemmer@gmail.com>
2020-12-18 22:41:19 -0500

    Fixed challenge seven

--- problems/code7.rb
+++ problems/code7.rb
@@ -13,3 +13,41 @@ require_relative 'helpers'  # DO NOT REMOVE THIS LINE
 
 # Step 4: Display the result
 
+class Calculator
+    attr_accessor :total
+
+    def initialize
+        @total = 0.0
+    end
+
+    def add(value)
+        @total = @total + value 
+    end
+
+    def subtract(value)
+        @total = @total - value 
+    end
+
+    def multiply(value)
+        @total = @total * value 
+    end
+
+    def divide(value)
+        @total = @total / value 
+    end
+    
+    def display(precision = 0)
+        tell_me @total.round(precision)
+    end
+end
+
+calc1 = Calculator.new
+calc1.add(5)
+calc1.multiply(78)
+calc1.divide(3)
+calc1.display
+
+calc2 = Calculator.new
+calc2.add(10)
+calc2.divide(3)
+#calc2.display
--- problems/solution7.rb
+++ problems/solution7.rb
@@ -4,7 +4,7 @@ class Calculator
     attr_accessor :total
 
     def initialize
-        @total = 0
+        @total = 0.0
     end
 
     def add(value)
@@ -23,13 +23,18 @@ class Calculator
         @total = @total / value 
     end
     
-    def display
-        tell_me @total
+    def display(precision = 0)
+        tell_me @total.round(precision)
     end
 end
 
-calc = Calculator.new
-calc.add(5)
-calc.multiply(78)
-calc.divide(3)
-calc.display
+calc1 = Calculator.new
+calc1.add(5)
+calc1.multiply(78)
+calc1.divide(3)
+calc1.display
+
+calc2 = Calculator.new
+calc2.add(10)
+calc2.divide(3)
+calc2.display(2)
